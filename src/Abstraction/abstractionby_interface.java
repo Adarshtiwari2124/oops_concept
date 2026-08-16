654598c524dd24f615d221456d11e3b9194bf17a
adarshtiwari2124 <madarshtiwari@gmail.com>
2026-08-16 14:55:15 +0530

    Added interface abstraction example

---
+++ src/Abstraction/abstractionby_interface.java
@@ -0,0 +1,16 @@
+interface I1 {
+   
+  public abstract void show();
+
+}
+   class Test implements I1{
+
+      public void show(){
+        System.out.println("1234");
+      }
+
+      public static void main(String[] args){
+        Test t = new Test();
+        t.show();
+      }
+   }
